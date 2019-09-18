8062af2216be93e402013dc538ad5d5faa6c76cf
Matthew C <m23.cullen@gmail.com>
2019-09-18 22:23:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,25 @@
 class Dog 
   attr_accessor :name 
   @@all = []
+  @@names = []
   
   def initialize(name)
     @name = name 
     @@all << self  
+    @@names << name 
   end 
   
   def self.all 
     @@all
   end 
   
+  def self.clear_all
+    @@all.clear
+  end 
   
+  def self.print_all
+    @@names.each do |name|
+      puts "#{name}"
+    end 
+  end 
 end 
